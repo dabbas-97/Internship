d43adde898610526e109a306ebf21655306f1abf
hamza <hamzeh.d_97@yahoo.com>
2019-10-12 12:18:48 +0300

    ...

--- src/Pages/Profile/Components/Student/UserInfo.js
+++ src/Pages/Profile/Components/Student/UserInfo.js
@@ -127,7 +127,9 @@ export default class UserInfo extends Component {
           </button>
         );
     };
-    const submitChanges = () => {};
+    const submitChanges = () => {
+      this.setState({ info: 'view' });
+    };
 
     return (
       <div className=' profileInfo'>
